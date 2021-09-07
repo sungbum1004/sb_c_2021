4c04fb9054347afadb0ed054edb6acadb9eac30e
sungbum1004 <tlsqkfka96@naver.com>
2021-09-07 10:55:14 +0900

    /usr/home/main2, /usr/home/main3 추가 / 호출 시 반환되는 값이 1씩 커지는 컨트롤러 액션 구현

--- src/main/java/com/jsb/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/jsb/exam/demo/controller/UsrHomeController.java
@@ -7,11 +7,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // @RequestMapping/@ResponseBody 메서드들을 품고 있으려면 붙여야함
 @Controller
 public class UsrHomeController {
+	private int count;
+
+	public UsrHomeController() {
+		count = -1;
+	}
+
 	// 받은편지 (usr/home/main의 요청이 들어옴)
 	@RequestMapping("/usr/home/main")
-	// 보낼편지 
+	// 보낼편지
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요.";
+	}
+
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또만나요";
+	}
+
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
 	}
 }
