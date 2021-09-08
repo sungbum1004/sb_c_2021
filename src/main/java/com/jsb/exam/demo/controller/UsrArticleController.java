1e6821450aea81d6522512d6ab090a136b766085
sungbum1004 <tlsqkfka96@naver.com>
2021-09-08 09:43:16 +0900

    ArticleService 추가

--- src/main/java/com/jsb/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jsb/exam/demo/controller/UsrArticleController.java
@@ -3,15 +3,19 @@ package com.jsb.exam.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.jsb.exam.demo.service.ArticleService;
 import com.jsb.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 	// 인스턴스 변수 시작
+	@Autowired
+	private ArticleService articleService;
 	private int articlesLastId;
 	private List<Article> articles;
 	// 인스턴스 변수 끝
---
+++ src/main/java/com/jsb/exam/demo/service/ArticleService.java
@@ -0,0 +1,8 @@
+package com.jsb.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+
+}
