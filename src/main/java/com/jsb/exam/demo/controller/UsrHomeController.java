11ac4aa656f477fa90fdfbb90ed64780a118e498
sungbum1004 <tlsqkfka96@naver.com>
2021-09-08 10:24:31 +0900

    UsrArticleController가 가지고 있던 서비스 로직들을 ArticleService로 옮김

--- src/main/java/com/jsb/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/jsb/exam/demo/controller/UsrHomeController.java
@@ -1,95 +1,14 @@
 package com.jsb.exam.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.jsb.exam.demo.vo.Article;
-
-//@RequestMapping/@ResponseBody 메서드들을 품고 있으려면 붙여야함
 @Controller
 public class UsrHomeController {
-	// 받은편지
-	@RequestMapping("/usr/home/getString")
-	// 보낼편지
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String getString() {
-		return "HI";
-	}
-
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-
-	@RequestMapping("/usr/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-
-	@RequestMapping("/usr/home/getCharacter")
-	@ResponseBody
-	public char getCharacter() {
-		return 'a';
-	}
-
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이", 22);
-		map.put("영희나이", 21);
-
-		return map;
-	}
-
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수");
-		list.add("영희");
-
-		return list;
-	}
-
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article artricle = new Article(1, "제목1", "내용1");
-
-		return artricle;
-	}
-	
-	@RequestMapping("/usr/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		Article artricle1 = new Article(1, "제목1", "내용2");
-		Article artricle2 = new Article(2, "제목2", "내용2");
-
-		List<Article> list = new ArrayList<>();
-		list.add(artricle1);
-		list.add(artricle2);
-
-		return list;
+		return "안녕하세요.";
 	}
 }
